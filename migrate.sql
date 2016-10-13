7b1d2fc96a2108792956ce6fc7e24d113ea1c519
Xavier Denis <xldenis@gmail.com>
2016-10-13 11:57:08 -0500

    Add sql migration

--- migrate.sql
+++ migrate.sql
@@ -2,6 +2,6 @@ create table posts (
   id serial primary key,
   title varchar(128) not null,
   body text not null,
-  created_At timestamp not null,
-  updated_at timestamp not null
+  created_at timestamptz not null,
+  updated_at timestamptz not null
 );
